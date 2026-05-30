712e58a40f1ceb099ba7bd9afbc99d64efe32bbe
Dhiraj Hegade <dhirajhegade10@gmail.com>
2026-05-30 13:21:54 +0530

    Refactor PdfService to format invoice date

--- src/main/java/com/bill/services/PdfService.java
+++ src/main/java/com/bill/services/PdfService.java
@@ -1,6 +1,7 @@
 package com.bill.services;
 
 import java.io.ByteArrayOutputStream;
+import java.time.format.DateTimeFormatter;
 
 import org.springframework.stereotype.Service;
 
@@ -12,38 +13,47 @@ import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 
 @Service
-public class PdfService {
+public class PdfService
+{
 
-    public byte[] generateInvoice(Bill bill) throws Exception {
+	public byte[] generateInvoice(Bill bill) throws Exception
+	{
 
-        Document document = new Document();
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
+		Document document = new Document();
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-        PdfWriter.getInstance(document, out);
-        document.open();
+		PdfWriter.getInstance(document, out);
+		document.open();
 
-        document.add(new Paragraph("Cafe Invoice"));
-        document.add(new Paragraph("Bill ID: " + bill.getId()));
-        document.add(new Paragraph("Date: " + bill.getBillDate()));
-        document.add(new Paragraph(" "));
+		// Date Format
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm a");
 
-        PdfPTable table = new PdfPTable(4);
-        table.addCell("Item");
-        table.addCell("Qty");
-        table.addCell("Price");
-        table.addCell("Total");
+		String formattedDate = bill.getBillDate().format(formatter);
 
-        for (BillItem item : bill.getItems()) {
-            table.addCell(item.getProductName());
-            table.addCell(String.valueOf(item.getQuantity()));
-            table.addCell(String.valueOf(item.getPrice()));
-            table.addCell(String.valueOf(item.getTotal()));
-        }
+		document.add(new Paragraph("Cafe Invoice"));
+		document.add(new Paragraph("Bill ID: " + bill.getId()));
+		document.add(new Paragraph("Date: " + formattedDate));
+		document.add(new Paragraph(" "));
 
-        document.add(table);
-        document.add(new Paragraph("Total Amount: " + bill.getTotalAmount()));
+		PdfPTable table = new PdfPTable(4);
+		table.addCell("Item");
+		table.addCell("Qty");
+		table.addCell("Price");
+		table.addCell("Total");
 
-        document.close();
-        return out.toByteArray();
-    }
+		for (BillItem item : bill.getItems())
+		{
+			table.addCell(item.getProductName());
+			table.addCell(String.valueOf(item.getQuantity()));
+			table.addCell(String.valueOf(item.getPrice()));
+			table.addCell(String.valueOf(item.getTotal()));
+		}
+
+		document.add(table);
+		document.add(new Paragraph(" "));
+		document.add(new Paragraph("Total Amount: ₹" + bill.getTotalAmount()));
+
+		document.close();
+		return out.toByteArray();
+	}
 }
